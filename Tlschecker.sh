4538e1c90b36e9280566029b073aac4aaf2a97ea
Srithar Cyber <119922855+SritharCyber@users.noreply.github.com>
2024-02-17 09:59:06 +0530

    Update Tlschecker.sh

--- Tlschecker.sh
+++ Tlschecker.sh
@@ -8,6 +8,7 @@ YELLOW='\033[1;33m'
 YELLOWB='\e[103m'
 RESET='\033[0m'
 BLUEB='\e[44m'
+blue='\033[1;34m'
 MAGENTA='\e[1;35m%s\e[0m\n'
 cyan='\e[96m'
 
@@ -33,7 +34,6 @@ if [ "$#" -eq 0 ]; then
 fi
 
 
-
 domain="$1"
 w0="Protocol  : TLSv1.1"
 w1="Protocol  : TLSv1.2"
@@ -44,8 +44,6 @@ banner
 echo "*********************************************"
 echo -e "       ${RED}Domain provided:${RESET} ${GREEN}$domain${RESET}" 
 echo "  *********************************************"
-
-
 
 
 output1=$(echo | gnutls-cli $domain | grep Connecting | awk '{print $3}') 
@@ -61,10 +59,11 @@ echo -e "${YELLOW}$output1${RESET}"
 printf "${GREEN}Your Domain SSL:" 
 echo -e "${YELLOW}$output4${RESET}"
 
-
 printf "${GREEN}Status:" 
 echo -e "${YELLOW}$output3${RESET}"
 printf '\n'
+
+
 echo """--------------------------------------------------"""
 echo -e "${RED}Your Domain Using Below Versions.${RESET} - ${GREEN}Vulnerability${RESET}"
 echo """--------------------------------------------------"""
@@ -82,60 +81,65 @@ v3=$(cat output3.log | grep "Protocol  : TLSv1.3")
 
 if [[ "$v1" =~ $w0 ]]; then
 
-  printf "TLSv1.1 is present.  - ${REDB}High${RESET}\n"
+  printf "TLSv1.1 is present.  - ${RED}High${RESET}\n"
 else
-   printf "TLSv1.1 not present. - ${BLUEB}Info${RESET}\n"
+   printf "TLSv1.1 not present. - ${blue}Info${RESET}\n"
 fi
 printf '\n'
 
 if [[ "$v2" =~ $w1 ]]; then
  
-  printf "TLSv1.2 is present.  - ${YELLOWB}Medium${RESET}\n"
+  printf "TLSv1.2 is present.  - ${YELLOW}Medium${RESET}\n"
 else
-  printf "TLSv1.2 not present. - ${GREENB}LOW${RESET}\n"
+  printf "TLSv1.2 not present. - ${GREEN}LOW${RESET}\n"
 fi
 printf '\n'
 if [[ "$v3" =~ $w2 ]]; then
  
-  printf "TLSv1.3 is present.  - ${BLUEB}Info${RESET}\n"
+  printf "TLSv1.3 is present.  - ${blue}Info${RESET}\n"
 else
- printf "TLSv1.3 not present. - ${REDB}High${RESET}\n"
+ printf "TLSv1.3 not present. - ${RED}High${RESET}\n"
 fi
-
-t=$(testssl -H $domain:443 | awk '/(CVE-2014-0160)/ {print; getline; print; getline; print}' | awk '!seen[$0]++')
-t1=$(testssl -I $domain:443 | awk '/(CVE-2014-0224)/ {print; getline; print; getline; print}' | awk '!seen[$0]++')
-t2=$(testssl -T $domain:443 | awk '/(CVE-2016-9244)/ {print; getline; print; getline; print}' | awk '!seen[$0]++')
-t3=$(testssl --BB $domain:443 | awk '/ROBOT / {print; getline; print; getline; print}' | awk '!seen[$0]++')
-t4=$(testssl -R $domain:443 | awk '/Secure/ {print; getline; print; getline; print}' | awk '!seen[$0]++')
-t5=$(testssl -C $domain:443 | awk '/(CVE-2012-4929)/ {print; getline; print; getline; print}' | awk '!seen[$0]++')
-t6=$(testssl -B $domain:443 | awk '/(CVE-2013-3587)/ {print; getline; print; getline; print}' | awk '!seen[$0]++')
-t7=$(testssl -O $domain:443 | awk '/(CVE-2014-3566)/ {print; getline; print; getline; print}' | awk '!seen[$0]++')
-#testssl -Z $domain:443 | awk '/RFC/ {print; getline; print; getline; print}' | awk '!seen[$0]++'
-t=8$(testssl -W $domain:443 | awk '/(CVE-2016-2183, CVE-2016-6329)/ {print; getline; print; getline; print}' | awk '!seen[$0]++')
-t=9$(testssl -A $domain:443 | awk '/(CVE-2011-3389)/ {print; getline; print; getline; print}' | awk '!seen[$0]++')
-t=10$(testssl -L $domain:443 | awk '/(CVE-2013-0169)/ {print; getline; print; getline; print}' | awk '!seen[$0]++')
-t=11$(testssl -WS $domain:443 | awk '/(CVE-2014-6321)/ {print; getline; print; getline; print}' | awk '!seen[$0]++')
-t=12$(testssl -F $domain:443 | awk '/(CVE-2015-0204)/ {print; getline; print; getline; print}' | awk '!seen[$0]++')
-t=13$(testssl -J $domain:443 | awk '/(CVE-2015-4000)/ {print; getline; print; getline; print}' | awk '!seen[$0]++')
-t=14$(testssl -D $domain:443 | awk '/(CVE-2016-0800, CVE-2016-0703)/ {print; getline; print; getline; print}' | awk '!seen[$0]++')
- printf "t\n"
- printf "t1\n"
- printf "t2\n"
- printf "t3\n"
- printf "t4\n"
- printf "t5\n"
- printf "t6\n"
- printf "t7\n"
- printf "t8\n"
- printf "t9\n"
- printf "t10\n"
- printf "t11\n"
- printf "t12\n"
- printf "t13\n"
- printf "t14\n"
+rm output.log output1.log output2.log output3.log
+printf "\n"
+echo """-------------------------------------------------------------------------------------------------------------------------------------------------"""
+echo -e "${GREEN}Vulnerabilities : ${RESET}"
+printf "\n"
+t=$(testssl.sh -H $domain:443 | awk '/(CVE-2014-0160)/ {print; getline; print; getline; print}' | awk '!seen[$0]++' | awk '!/Done/')
+printf "$t\n"
+t1=$(testssl.sh -I $domain:443 | awk '/(CVE-2014-0224)/ {print; getline; print; getline; print}' | awk '!seen[$0]++' | awk '!/Done/')
+printf "$t1\n"
+t2=$(testssl.sh -T $domain:443 | awk '/(CVE-2016-9244)/ {print; getline; print; getline; print}' | awk '!seen[$0]++' | awk '!/Done/')
+printf "$t2\n"
+t3=$(testssl.sh -BB $domain:443 | awk '/ROBOT / {print; getline; print; getline; print}' | awk '!seen[$0]++' | awk '!/Done/')
+printf "$t3\n"
+t4=$(testssl.sh -R $domain:443 | awk '/Secure/ {print; getline; print; getline; print}' | awk '!seen[$0]++' | awk '!/Done/')
+printf "$t4\n"
+t5=$(testssl.sh -C $domain:443 | awk '/(CVE-2012-4929)/ {print; getline; print; getline; print}' | awk '!seen[$0]++' | awk '!/Done/')
+printf "$t5\n"
+t6=$(testssl.sh -B $domain:443 | awk '/(CVE-2013-3587)/ {print; getline; print; getline; print}' | awk '!seen[$0]++' | awk '!/Done/')
+printf "$t6\n"
+t7=$(testssl.sh -O $domain:443 | awk '/(CVE-2014-3566)/ {print; getline; print; getline; print}' | awk '!seen[$0]++' | awk '!/Done/' | awk '!/ Testing for TLS_FALLBACK_SCSV Protection/')
+printf "$t7\n"
+t15=$(testssl.sh -Z $domain:443 | awk '/RFC/ {print; getline; print; getline; print}' | awk '!seen[$0]++' | awk '!/Done/' )
+printf "$t15\n"
+t8=$(testssl.sh -W $domain:443 | awk '/(CVE-2016-2183, CVE-2016-6329)/ {print; getline; print; getline; print}' | awk '!seen[$0]++' | awk '!/Done/')
+printf "$t8\n"
+t9=$(testssl.sh -A $domain:443 | awk '/(CVE-2011-3389)/ {print; getline; print; getline; print}' | awk '!seen[$0]++' | awk '!/Done/')
+printf "$t9\n"
+t10=$(testssl.sh -L $domain:443 | awk '/(CVE-2013-0169)/ {print; getline; print; getline; print}' | awk '!seen[$0]++' | awk '!/Done/')
+printf "$t10\n"
+t11=$(testssl.sh -W $domain:443 | awk '/(CVE-2014-6321)/ {print; getline; print; getline; print}' | awk '!seen[$0]++' | awk '!/Done/')
+printf "$t11\n"
+t12=$(testssl.sh -F $domain:443 | awk '/(CVE-2015-0204)/ {print; getline; print; getline; print}' | awk '!seen[$0]++' | awk '!/Done/')
+printf "$t12\n"
+t13=$(testssl.sh -J $domain:443 | awk '/(CVE-2015-4000)/ {print; getline; print; getline; print}' | awk '!seen[$0]++' | awk '!/Done/') 
+printf "$t13\n"
+t14=$(testssl.sh -D $domain:443 | awk '/(CVE-2016-0800, CVE-2016-0703)/ {print; getline; print; getline; print}' | awk '!seen[$0]++' | awk '!/Done/')
+printf "$t14\n"
 
 printf '\n'
-echo """--------------------------------------------------"""
-rm output.log output1.log output2.log output3.log
+echo """-------------------------------------------------------------------------------------------------------------------------------------------------"""
+
 echo -e ""
-echo -e "${RED}               ✿  ${RESET} ${GREEN} Scan completed. ${RESET} ${RED}  ✿ ${RESET}"
+echo -e "${RED}                              ✿  ${RESET} ${GREEN} Scan completed. ${RESET} ${RED}  ✿ ${RESET}"
